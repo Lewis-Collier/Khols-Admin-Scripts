ca268b6df2769dd87d3314670a8057cc952417f5
Lewis-Collier <112239446+Lewis-Collier@users.noreply.github.com>
2023-08-04 11:58:13 +0100

    Update Crasher.lua

--- Crasher.lua
+++ Crasher.lua
@@ -2,11 +2,23 @@ game.Players:Chat("gear me 94794847")
 game.Players:Chat("h equip to crash server, make sure u have no other tools or it wont work. (yes this script sucks)")
 wait(1.2)
 repeat
-wait()
-until #game.Players.LocalPlayer.Backpack:GetChildren() == 0 and game.Players.LocalPlayer.Character:FindFirstChild("Vampire Vanquisher")
+  wait()
+  if #game.Players.LocalPlayer.Backpack:GetChildren() == 0 and game.Players.LocalPlayer.Character:FindFirstChild("Vampire Vanquisher") then
+    return
 wait(1)
 game.Players:Chat("m cya.")
 wait(1.2)
 game.Players:Chat("size me 0.3")
 game.Players:Chat("size me 0.3")
 game.Players:Chat("size me 0.3")
+  end
+until #game.Players.LocalPlayer.Backpack:GetChildren() == 0 and game.Players.LocalPlayer.Character:FindFirstChild("Vampire Vanquisher")
+wait(0.5)
+if #game.Players.LocalPlayer.Backpack:GetChildren() == 0 and game.Players.LocalPlayer.Character:FindFirstChild("Vampire Vanquisher") then
+    wait(1)
+    game.Players:Chat("m cya.")
+    wait(1.2)
+    game.Players:Chat("size me 0.3")
+    game.Players:Chat("size me 0.3")
+    game.Players:Chat("size me 0.3")
+end

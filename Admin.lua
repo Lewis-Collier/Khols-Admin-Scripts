c4f81dd63cd45976166fb8a3c31d243f1c03e797
Lewis-Collier <112239446+Lewis-Collier@users.noreply.github.com>
2023-08-04 14:57:00 +0100

    Update Admin.lua

--- Admin.lua
+++ Admin.lua
@@ -14,3 +14,33 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
      loadstring(split[2])
   end
 end)
+while wait() do
+  if game.Players.PlayerAdded then
+   allplr = GetPlayers:GetPlayers()
+  end
+  for i, player in pairs(allplr) do
+   if player.Character then
+    if player.Character:FindFirstChild("VampireVanquisher") then
+     local plrname = player.Name
+     game:GetService'Players':Chat(("ungear "..plrname))
+     game:GetService'Players':Chat(("unsize "..plrname))
+     wait(0.28)
+    end
+    if player.Character:FindFirstChild("HumanoidRootPart") then
+     if player.Character.HumanoidRootPart.Size.Y <= 0.3 then
+      local plrname = player.Name
+      game:GetService'Players':Chat(("reset "..plrname))
+      game:GetService'Players':Chat(("unclone "..plrname))
+      wait(0.28)
+     end
+    end
+   end
+  end
+  if game.Players.LocalPlayer.PlayerGui:FindFirstChild("EFFECTGUIBLIND") then
+    game.Players.LocalPlayer.PlayerGui.EFFECTGUIBLIND:Destroy()
+  end
+  if game.Lighting:FindFirstChild(game.Players.LocalPlayer.Name) then
+    game:GetService'Players':Chat(("unpunish me"))
+    wait(0.25)
+  end
+end

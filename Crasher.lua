17c51e39516d68c244efe8f73d707af61622067f
Lewis-Collier <112239446+Lewis-Collier@users.noreply.github.com>
2023-08-04 11:36:19 +0100

    Update Crasher.lua

--- Crasher.lua
+++ Crasher.lua
@@ -1,12 +1,12 @@
 game.Players:Chat("gear me 94794847")
-game.Players:Chat("h equip to crash server, make sure u have no other tools or it wont work")
+game.Players:Chat("h equip to crash server, make sure u have no other tools or it wont work. (yes this script sucks)")
 wait(1.2)
 repeat
 wait()
 until #game.Players.LocalPlayer.Backpack:GetChildren() == 0
 wait(1)
-game.Players:Chat("m say bye to ur server, get this script at bit.ly/khols-crasher")
-wait(0.8)
+game.Players:Chat("m say bye to ur server, get this script at bit.ly/khols-crasher.")
+wait(1.2)
 game.Players:Chat("size me 0.3")
 game.Players:Chat("size me 0.3")
 game.Players:Chat("size me 0.3")

